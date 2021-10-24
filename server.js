6fdd8970baeeabf3eae9550bd5c9a5e795a3c3f8
Ida Darmochwał <ida.mdart@gmail.com>
2021-10-24 23:05:33 +0200

    Edit helmet config

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
